295abcb5beadf383fd9fa3d0e1d7925a0d140adf
smellinor <43417011+smellinor@users.noreply.github.com>
2018-10-31 22:24:51 +0100

    Rename P3.1.java to Chapter 3/P3.1.java

---
+++ Chapter 3/P3.1.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+
+/*
+ * Write a program that reads an integer and prints
+ * whether it is negative, zero or positive.
+ *
+ */
+
+public class P3.1 {
+
+	
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("Enter an integer: ");
+		
+		int inputInt = in.nextInt();
+		
+		in.close();
+
+		
+		
+		ifinputInt > 0) {
+			
+			System.out.println("The integer is positive");
+		
+		} else if inputInt < 0) {
+			
+			System.out.println("The integer is negative");
+		
+		} else {
+			
+			System.out.println("The integer is 0");
+		
+		}
+
+	}
+
+
+}
