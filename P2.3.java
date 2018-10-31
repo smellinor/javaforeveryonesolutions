22d0c1421fb011e035ade9538f6c6b0535ed178d
smellinor <43417011+smellinor@users.noreply.github.com>
2018-10-31 21:08:53 +0100

    Add files via upload

---
+++ P2.3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/*
+ * Write a program that reads a number and displays the square,
+ * cube and fourth power. Use the Math.pow method only for the fourth power.
+ */
+public class P2.3 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter an integer");
+		int x = in.nextInt();
+		in.close();
+		
+		System.out.println("Square: " + x*x);
+		System.out.println("Cube: " + x*x*x);
+		System.out.println("Fourth power: " + Math.pow(x,4));
+		
+	}
+}
